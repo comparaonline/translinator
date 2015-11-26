3a93a277f2e0a52cea2e16297b6986012b2015dc
Mario Negrete <txus2k@gmail.com>
2015-11-25 23:04:59 -0300

    Search for children path

--- app/controllers/translinator/textinator_callback.rb
+++ app/controllers/translinator/textinator_callback.rb
@@ -1,13 +1,11 @@
 module Translinator
   module TextinatorCallback
     def translate!(search = {})
-      @tl = Textinator.get(
-        {
-          application: Rails.application.class.parent_name.downcase,
-          countryCode: I18n.locale,
-          url: request.original_fullpath
-        }.merge(search)
-      )
+      @tl = Textinator.texts({
+        application: Rails.application.class.parent_name.downcase,
+        countryCode: I18n.locale,
+        url: request.original_fullpath
+      }.merge(search))
     end
   end
 end
--- lib/translinator/textinator.rb
+++ lib/translinator/textinator.rb
@@ -1,17 +1,27 @@
 module Translinator
   class Textinator
     class << self
-      def get(params)
-        response = RestClient::Request.execute(
-          method: :get,
-          url: CoConfig::TEXTINATOR_API[:text_url],
-          timeout: 10,
-          open_timeout: 10
-        )
+      TIMEOUT = 100
+
+      def texts(params)
+        father_path = params[:url]
+        children_path(father_path).inject({}) do |texts, path|
+          texts.merge(get_texts(params.merge(url: path)))
+        end
+      end
+
+      def get_texts(params)
+        resource = RestClient::Resource.new(CoConfig::TEXTINATOR_API[:text_url], timeout: TIMEOUT, open_timeout: TIMEOUT)
+        response = resource.get(params: params)
         texts = JSON.parse(response)
         Hash[texts.map { |t| [t['key'], t['value']] }].with_indifferent_access
       rescue StandardError => e
         {}
+      end
+
+      def children_path(url)
+        url = url.split('/')
+        url.map.with_index { |u, i| url.dup.drop(i).reverse.join('/') }.reverse.drop(1)
       end
     end
   end
