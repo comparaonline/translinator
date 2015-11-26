70332d80723bd6a4f65000f4d795f3f59f9259e7
Mario Negrete <txus2k@gmail.com>
2015-11-26 16:52:36 -0300

    Mini refactor
    Gets text by path
    Default I18n

--- app/helpers/translinator/textinator_helper.rb
+++ app/helpers/translinator/textinator_helper.rb
@@ -2,8 +2,11 @@ module Translinator
   module TextinatorHelper
     def tl(key, options = {})
       text = @tl.try(:[], key)
-      return (text % options).html_safe if text
-      I18n.t(key, options)
+      if text.blank?
+        return I18n.t(key, options) if defined?(I18n)
+        return options.try(:[], :default) || ''
+      end
+      (text % options).html_safe
     end
   end
 end
--- lib/translinator/textinator.rb
+++ lib/translinator/textinator.rb
@@ -1,19 +1,21 @@
 module Translinator
   class Textinator
-    class << self
-      TIMEOUT = 100
+    REQUEST_TIMEOUT = 100
 
+    class << self
       def texts(params)
-        children_path(params[:url]).inject({}) do |t, path|
-          t.merge(get_texts(params.merge(url: path)))
+        path_childen = path_childen_by_priority(params[:url])
+        path_childen.inject({}) do |all_texts, path|
+          texts_path = get_texts(params.merge(url: path))
+          all_texts.merge(texts_path)
         end
       end
 
       def get_texts(params)
         resource = RestClient::Resource.new(
           CoConfig::TEXTINATOR_API[:text_url],
-          timeout: TIMEOUT,
-          open_timeout: TIMEOUT
+          timeout: REQUEST_TIMEOUT,
+          open_timeout: REQUEST_TIMEOUT
         )
         response = resource.get(params: params)
         texts = JSON.parse(response)
@@ -22,12 +24,32 @@ module Translinator
         {}
       end
 
-      def children_path(url)
-        url = url.split('/').reverse
-        url.map.with_index do |u, i|
-          url.dup.drop(i).reverse.join('/')
-        end.reverse.drop(1)
+      private
+
+      def path_childen_by_priority(path)
+        path_children(path).reverse.drop(1)
       end
+
+
+      def path_children(path)
+        path_segments = path_segments(path)
+        path_segments.map.with_index do |_, index|
+          build_child_path(path_segments, index)
+        end
+      end
+
+      def build_child_path(path_segments, index)
+        delete_segment(path_segments, index).reverse.join('/')
+      end
+
+      def path_segments(path)
+        path.split('/').reverse
+      end
+
+      def delete_segment(path, index)
+        path.dup.drop(index)
+      end
+
     end
   end
 end
