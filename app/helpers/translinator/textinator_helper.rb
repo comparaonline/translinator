56fd625099e5e8cd0bf8aecf4069cc29d90e6a95
Mario Negrete <txus2k@gmail.com>
2015-12-02 23:38:03 -0300

    Add editable content tag

--- app/helpers/translinator/textinator_helper.rb
+++ app/helpers/translinator/textinator_helper.rb
@@ -1,12 +1,23 @@
 module Translinator
   module TextinatorHelper
-    def tl(key, options = {})
+    include ActionView::Helpers::TagHelper
+
+    def tl(key, options = {}, meta_tag = false)
       text = @tl.try(:[], key)
       if text.blank?
-        return I18n.t(key, options) if defined?(I18n)
-        return options[:default] || ''
+        text ||= I18n.t(key, options) if defined?(I18n)
+        text ||= (options[:default] || '')
+      else
+        text = (text % options).html_safe
       end
-      (text % options).html_safe
+      if params.try(:[], :'tl-editable') && !key.include?('seo') && !meta_tag
+        return editable_content_tag(text)
+      end
+      text
+    end
+
+    def editable_content_tag(html)
+      content_tag(:span, html, class: 'tl-editable')
     end
   end
 end
