1d18bfa43ebe5cf851004771997c0908088f85e0
Mario Negrete <txus2k@gmail.com>
2015-12-09 18:34:44 -0300

    Fix get all texts

--- lib/translinator/textinator.rb
+++ lib/translinator/textinator.rb
@@ -2,7 +2,7 @@ module Translinator
   class Textinator
     class << self
       def texts(params)
-        response = {}.t_s
+        response = {}.to_s
         Timeout.timeout(CoConfig::TEXTINATOR_API[:request_timeout]) do
           response = RestClient.get(
             CoConfig::TEXTINATOR_API[:text_url],
@@ -10,7 +10,10 @@ module Translinator
           )
         end
         JSON.parse(response).with_indifferent_access
-      rescue StandardError => e
+      rescue StandardError => exception
+        Rails.logger.error "Textinator error on fetching all texts: with params:#{params.inspect}:"\
+        + " #{exception.message}\n"\
+        + exception.backtrace.join("\n")
         {}
       end
     end
