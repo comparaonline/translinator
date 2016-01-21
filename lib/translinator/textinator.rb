76e6ded047e734db5a99bd6ced47a1c3d3558f2a
Mario Negrete <txus2k@gmail.com>
2016-01-21 19:20:28 -0300

    Options for translate from app

--- lib/translinator/textinator.rb
+++ lib/translinator/textinator.rb
@@ -1,7 +1,18 @@
 module Translinator
   class Textinator
     class << self
-      def texts(params)
+
+      def texts(request, search = {})
+        fetch_texts({
+          application: Rails.application.class.parent_name.downcase,
+          countryCode: defined?(I18n) ? I18n.locale : :cl,
+          url: request.original_fullpath
+        }.merge(search))
+      end
+
+      private
+
+      def fetch_texts(params)
         response = {}.to_s
         Timeout.timeout(CoConfig::TEXTINATOR_API[:request_timeout]) do
           response = RestClient.get(
@@ -11,11 +22,13 @@ module Translinator
         end
         JSON.parse(response).with_indifferent_access
       rescue StandardError => exception
-        Rails.logger.error "Textinator error on fetching all texts: with params:#{params.inspect}:"\
+        Rails.logger.error "Textinator error on fetching all texts:\n"\
+        + " with params: #{params.inspect}\n"\
         + " #{exception.message}\n"\
         + exception.backtrace.join("\n")
         {}
       end
+
     end
   end
 end
