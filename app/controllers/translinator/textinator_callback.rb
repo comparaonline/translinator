fe718cd496a1eb4ac00fec9c2db4bc21aa22a243
Mario Negrete <txus2k@gmail.com>
2016-01-22 11:05:04 -0300

    unless instead if !

--- app/controllers/translinator/textinator_callback.rb
+++ app/controllers/translinator/textinator_callback.rb
@@ -1,7 +1,7 @@
 module Translinator
   module TextinatorCallback
     def translate!(options = {})
-      return true if !Translinator::Config.enabled?
+      return true unless Translinator::Config.enabled?
 
       @tl = Textinator.texts(request, options[:search] || {})
       @tl_app_vars = options[:app_vars] || {}
